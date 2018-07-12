43c3ba97336be785337e78fb2341d0089b4378dc
julianhocker <julian.hocker@dipf.de>
2018-07-12 16:42:31 +0200

    update documentation

--- R/rescale.R
+++ R/rescale.R
@@ -1,6 +1,8 @@
 #' Rescaling vectors
 #'
 #' @param v numeric vectors
+#' @param lower numeric value of lower rescaling value default: 0
+#' @param upper numeric value of upper rescaling value default: 1
 #'
 #' @return numeric vecotr, rescaled from input vector to the range of 0-1
 #' @export
@@ -8,6 +10,7 @@
 #' @examples
 #' rescale(c(1,2,3)) --> 0.0 0.5 1.0
 #' rescale(c(1,2,3,4,5)) --> 0.00 0.25 0.50 0.75 1.00
+#' rescale(c(1,2,3), 1,2) --> 1.0 1.5 2.0
 rescale <- function(v, upper =1, lower= 0) {
   L <- min(v)
   H <- max(v)
