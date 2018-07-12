1422f032cbdc02221bbabea724ba053adb94cdbf
julianhocker <julian.hocker@dipf.de>
2018-07-12 14:43:55 +0200

    update description

--- R/center.R
+++ R/center.R
@@ -1,14 +1,14 @@
 ##centeR ;)
 
 
-(dat <- read.csv(file = "inflammation.csv", header = FALSE)) ##round brackets make you see the output directly
-
-head(dat)
-
-
-avg_patient_inflammation <- rowMeans(dat)
-avg_day_inflammation <- colMeans(dat)
-plot(avg_day_inflammation)
+# (dat <- read.csv(file = "inflammation.csv", header = FALSE)) ##round brackets make you see the output directly
+#
+# head(dat)
+#
+#
+# avg_patient_inflammation <- rowMeans(dat)
+# avg_day_inflammation <- colMeans(dat)
+# plot(avg_day_inflammation)
 
 #' Centering Datasets
 #'
@@ -17,19 +17,19 @@ plot(avg_day_inflammation)
 #'
 #' @return this function returns a new vector containing the original _data_ centered around the _desired_ values
 #'
-#' @examples 
+#' @examples
 #' center(c(1,2,3),0) -> -1 0 1
 center <- function(data, desired) {
   new_dat <- (data - mean(data)) + desired
   return(new_dat)
 }
 
-z <- c(1,2,3)
-
-center(z,0) #calculate the center for our dataset z
-centered <- center(dat[,4],desired = 0) #show 4th coloum
-##check if our function is right:
-sd(dat[,4])
-sd(centered)
-
-identical(x = sd(dat[,4]), y = sd(centered))
+# z <- c(1,2,3)
+#
+# center(z,0) #calculate the center for our dataset z
+# centered <- center(dat[,4],desired = 0) #show 4th coloum
+# ##check if our function is right:
+# sd(dat[,4])
+# sd(centered)
+#
+# identical(x = sd(dat[,4]), y = sd(centered))
